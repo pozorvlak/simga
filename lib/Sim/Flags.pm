1d5d6abad9ea7503d715c151aa344ffede972e69
Miles Gould <mgould1@inf.ed.ac.uk>
2011-07-25 15:34:39 +0100

    Factored out list of flag names from ecc_args.

--- lib/Sim/Flags.pm
+++ lib/Sim/Flags.pm
@@ -2,27 +2,33 @@ package Sim::Flags;
 require Exporter;
 @ISA = qw(Exporter);
 @EXPORT_OK = qw(ecc_args);
+use Carp;
+
+my @flag_names = (
+	"-bt",
+	"-bw_noreturn",
+	"-bw_non_equal",
+	"-bw_pointer",
+	"-bw_positive",
+	"-bw_float",
+	"-bw_call",
+	"-bw_neg_return",
+	"-bw_null_return",
+	"-bw_const_return",
+	"-bw_loop_header",
+	"-bw_loop_branch",
+	"-bw_loop_exit",
+);
+
 
 # Given a list of genes (numbers in range 0...$scale), get corresponding args
 # to hand to ecc.
 sub ecc_args {
 	my $granularity = shift;
 	my @genes = map { $_ * $granularity } @_;
-	return join " ", (
-		"-bt", $genes[0],
-		"-bw_noreturn", $genes[1],
-		"-bw_non_equal", $genes[2],
-		"-bw_pointer", $genes[3],
-		"-bw_positive", $genes[4],
-		"-bw_float", $genes[5],
-		"-bw_call", $genes[6],
-		"-bw_neg_return", $genes[7],
-		"-bw_null_return", $genes[8],
-		"-bw_const_return", $genes[9],
-		"-bw_loop_header", $genes[10],
-		"-bw_loop_branch", $genes[11],
-		"-bw_loop_exit", $genes[12],
-	);
+	carp "Expected ".scalar(@flag_names)." genes, got ".scalar(@genes)
+		unless $#flag_names == $#genes;
+	return join(" ", map { $flag_names[$_], $genes[$_] } (0 .. $#genes));
 }
 
 # The inverse function. Given a value of ECC_CC_FLAGS, give the genes that gave
