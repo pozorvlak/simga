0e01a1c839a14faf4977431aa5095ac8d915b64f
Miles Gould <mgould1@inf.ed.ac.uk>
2011-07-25 17:18:54 +0100

    generations script now picks out generations.

--- generations.pl
+++ generations.pl
@@ -4,27 +4,38 @@ use strict;
 use warnings;
 use Sim::Generations;
 use Getopt::Long;
+use Data::Dumper;
 
 sub usage {
-	print "Usage: $0 [--population p] [--backup backup_dir] logfile.ga";
+	print <<END;
+  Usage: $0
+    [--population p (default 50)]
+    [--backup backup_dir (default "backup")]
+    [--prefix prefix_for_generation_files ]
+    [--granularity g (default 0.01) ]
+    logfile.ga
+END
+	exit 1;
 }
 
 sub getopts {
-	if ($ARGC < 1) {
+	if (@ARGV < 1) {
 		usage();
 	}
-	my $logfile = $ARGV[0];
-	my $population = 50;
-	(my $backup_dir = "backup.$logfile") =~ s/.ga$//;
+	my ($population, $backup_dir, $prefix);
+	# (my $backup_dir = "backup.$logfile") =~ s/.ga$//;
+	# my $prefix = "$backup_dir/generation_";
+	$population = 50;
+	my $granularity = 0.01;
 	GetOptions("population=i" => \$population,
 		   "backup=s" => \$backup_dir,
-		   "prefix=s" => );
-	return ($logfile, $population, $backup_dir, $prefix);
+		   "prefix=s" => \$prefix,
+		   "granularity=i" => \$granularity);
+	my $logfile = $ARGV[0];
+	return ($granularity, $logfile, $population, $backup_dir, $prefix);
 }
 
-my ($logfile, $population, $backup_dir) = getopts();
+my ($granularity, $logfile, $population, $backup_dir) = getopts();
 my @generations = Sim::Generations::generations(
-			logfile => $ARGV[0],
-			population => $population,
-			backup_dir => $backup_dir);
-
+	$granularity, $logfile, $population, $backup_dir);
+print Dumper(\@generations);
