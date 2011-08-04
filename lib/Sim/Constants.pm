251a2e6578516f84d369a468e8e53af6fed664e9
Miles Gould <mgould1@inf.ed.ac.uk>
2011-08-04 18:41:54 +0100

    Simga didn't basically work; replace lexicals with package vars in Constants.

--- lib/Sim/Constants.pm
+++ lib/Sim/Constants.pm
@@ -1,10 +1,10 @@
 package Sim::Constants;
 
-my $bmark = "eembc2";
-my $bmark_dir = "EEMBC/eembc-2.0";
-my $scale = 100; # we represent [0,1] as {0,1,2,...$scale}
-my $granularity = 1/$scale; # granularity of gene variation
-my $num_genes = 13;
-my $logfilename = "sim.out";
+our $bmark = "eembc2";
+our $bmark_dir = "EEMBC/eembc-2.0";
+our $scale = 100; # we represent [0,1] as {0,1,2,...$scale}
+our $granularity = 1/$scale; # granularity of gene variation
+our $num_genes = 13;
+our $logfilename = "sim.out";
 
 1;
