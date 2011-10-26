d3f13916eb385d401cfb9dbc27145d25aaabcbf5
Miles Gould <mgould1@inf.ed.ac.uk>
2011-10-26 16:35:34 +0100

    Move fitness function out into its own module.

--- simga.pl
+++ simga.pl
@@ -5,25 +5,18 @@ use warnings;
 
 use AI::Genetic;
 use List::Util qw/sum/;
-use File::Find;
 use Regexp::Common;
 use Cwd qw/abs_path getcwd/;
 use Getopt::Long;
-use Sim::LogReader qw/cycles_from_log energy_from_log/;
-use Sim::Flags qw/ecc_args/;
-use Sim::Backup qw/backup genes_to_dirname/;
 use Sim::Constants;
+use Sim::Fitness qw/make_fitness/;
+use Sim::Backup qw/genes_to_dirname/;
 use Math::BigInt;
 
-my $bmark = $Sim::Constants::bmark;
-my $bmark_dir = $Sim::Constants::bmark_dir;
 my $scale = $Sim::Constants::scale;
-my $granularity = $Sim::Constants::granularity;
 my $num_genes = $Sim::Constants::num_genes;
 $| = 1;
 my $root_dir = getcwd();
-my $logfilename = $Sim::Constants::logfilename;
-
 my $generations = 10;
 my $population = 50;
 my $nobuild = 0;
@@ -37,6 +30,15 @@ GetOptions ("generations=i" => \$generations,
 if ($help) { usage(); }
 
 my $ecc_flags = $ENV{ECC_CC_FLAGS};
+
+my $options = {
+	root_dir => $root_dir,
+	generations => $generations,
+	population => $population,
+	nobuild => $nobuild,
+	optimize_for_cycles => $optimize_for_cycles,
+	ecc_flags => $ecc_flags,
+};
 
 sub usage {
 	print <<END;
@@ -54,43 +56,11 @@ END
 	exit 0;
 }
 
-# Find the total cost of running the benchmarks with a given set of genes.
-# Backs up the individual log-files as a side-effect.
-sub sum_cost {
-	my @genes = @_; # needed to provide sensible error messages downstream
-	my $cost = 0;
-	find(sub {
-		if ($_ eq $logfilename) {
-			if ($optimize_for_cycles) {
-				$cost += cycles_from_log($logfilename, $File::Find::name, @genes);
-			} else {
-				$cost += energy_from_log($logfilename, $File::Find::name, @genes);
-			}
-                        # passing in $_ leads to first arg being undefined...
-			backup $root_dir, $logfilename, $File::Find::name, @genes;
-		}
-	}, $bmark_dir);
-	return $cost;
-}
-
-# Fitness function. Build and run benchmarks with branch-prediction
-# flags set appropriately, collect energy usage.
-sub fitness {
-	my @genes = @{$_[0]};
-	$ENV{ECC_CC_FLAGS} = $ecc_flags . " " . ecc_args($granularity, @genes);
-	print "ECC_CC_FLAGS='$ENV{ECC_CC_FLAGS}'\n";
-	my $ran_cleanly = 0;
-	unless ($nobuild) {
-		system "make -j distclean-$bmark";
-		$ran_cleanly = (system("make -j run-$bmark") == 0);
-	}
-	return $ran_cleanly ? 1/(sum_cost(@genes) + 1) : 0;
-}
 
 my @ranges = ([0, $scale]) x ($num_genes - 1);
 unshift @ranges, [0.5 * $scale, $scale]; # threshold > 0.5
 my $ga = new AI::Genetic(
-	-fitness => \&fitness,
+	-fitness => make_fitness($options),
 	-type => 'rangevector',
 	-population => $population,
 );
